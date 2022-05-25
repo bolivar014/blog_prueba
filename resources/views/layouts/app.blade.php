bc246a044f929875925f5b330f17ff405a263b60
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-05-25 18:16:58 -0500

    integrando vista | perfil admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,6 +69,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/admins') }}">
+                                        {{ __('Admin') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// ------------------ CONTROLADORES DE VISTAS USUARIOS ------------------
 // Accedo al recurso compartido de los posts
 Route::resource('/posts', PostController::class);
-
+// Controlador para crear nuevo post
 Route::post('/posts/storePost', [App\Http\Controllers\PostController::class, 'storePost'])->name('posts.storePost');
+
+
+// ------------------ CONTROLADORES DE VISTAS ADMINS ------------------
+// Controlador de recursos compartidos para vistas admin
+Route::resource('/admins', AdminController::class);
