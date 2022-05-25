0bca631b52b8e4f0597577f3998d2d40993b81ee
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-05-25 14:56:37 -0500

    integrando middleware de autenticación

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 class PostController extends Controller
 {
+    // Integrando middleware de auth
+    public function __construct() {
+        return $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
